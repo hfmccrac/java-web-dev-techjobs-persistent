2e2f098c87cd3187ba60df65560c8d33c6770eea
Heather McCracken <hfmccracken@gmail.com>
2021-03-31 01:21:36 -0500

    Part 2 is DONE!

--- queries.sql
+++ queries.sql
@@ -7,6 +7,10 @@ String skills
 
 ## Part 2: Test it with SQL
 
+SELECT name
+FROM employer
+WHERE location="St. Louis City";
+
 ## Part 3: Test it with SQL
 
 ## Part 4: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -21,4 +21,6 @@ public class Skill extends AbstractEntity {
     public void setDescription(String description) {
         this.description = description;
     }
+
+
 }
